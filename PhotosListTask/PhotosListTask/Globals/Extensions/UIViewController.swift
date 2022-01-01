7c1013ddb86e8070565c7c0c77c14d6550b85c16
Aya Hilal <ahilal@identity-solutions.org>
2022-01-01 19:58:24 +0200

    Add UIViewController extension

---
+++ PhotosListTask/PhotosListTask/Globals/Extensions/UIViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController.swift
+//  PhotosListTask
+//
+//  Created by Aya Mohamed Hilal on 01/01/2022.
+//
+
+import Foundation
+import  UIKit
+
+extension UIViewController {
+    var isModal: Bool {
+        if let index = navigationController?.viewControllers.firstIndex(of: self), index > 0 {
+            return false
+        } else if presentingViewController != nil {
+            return true
+        } else if navigationController?.presentingViewController?.presentedViewController == navigationController {
+            return true
+        } else if tabBarController?.presentingViewController is UITabBarController {
+            return true
+        } else {
+            return false
+        }
+    }
+}
