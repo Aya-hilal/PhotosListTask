dde4a306610073c6c2bdcf0f7253671af95d2ef0
Aya Hilal <ahilal@identity-solutions.org>
2022-01-01 19:38:06 +0200

    Create application layer and app config file

---
+++ PhotosListTask/PhotosListTask/Application/AppConfiguration.swift
@@ -0,0 +1,30 @@
+//
+//  AppConfiguration.swift
+//  PhotosListTask
+//
+//  Created by Aya Mohamed Hilal on 01/01/2022.
+//
+
+import Foundation
+class AppConfiguration {
+    static var apiKey: String = {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ApiKey") as? String else {
+            fatalError("ApiKey must not be empty in plist")
+        }
+        return apiKey
+    }()
+    
+    static var apiBaseURL: String = {
+        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return apiBaseURL
+    }()
+    
+    static var imagesBaseURL: String = {
+        guard let imageBaseURL = Bundle.main.object(forInfoDictionaryKey: "ImageBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return imageBaseURL
+    }()
+}
