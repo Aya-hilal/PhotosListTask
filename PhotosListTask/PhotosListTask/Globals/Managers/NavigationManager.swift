5af2bbe5ddb76398fa907e743c14283d31506650
Aya Hilal <ahilal@identity-solutions.org>
2022-01-01 19:56:44 +0200

    Add navigation manager

---
+++ PhotosListTask/PhotosListTask/Globals/Managers/NavigationManager.swift
@@ -0,0 +1,34 @@
+//
+//  NavigationManager.swift
+//  PhotosListTask
+//
+//  Created by Aya Mohamed Hilal on 01/01/2022.
+//
+
+import Foundation
+import UIKit
+
+class NavigationManager {
+
+    static let sharedInstance = NavigationManager()
+    
+    private func openView(parentview: BaseViewController?, targetView: BaseViewController, returnNavigationController: Bool) {
+        guard let parentview = parentview else {
+            let navigationController = BaseNavigationController(rootViewController: targetView)
+            navigationController.modalPresentationStyle = .fullScreen
+            UIApplication.shared.windows.first?.rootViewController = navigationController
+            UIApplication.shared.windows.first?.makeKeyAndVisible()
+            return
+        }
+        if returnNavigationController {
+            let navigationController = BaseNavigationController(rootViewController: targetView)
+            navigationController.modalPresentationStyle = .fullScreen
+            parentview.present(navigationController, animated: true, completion: nil)
+        } else {
+            guard let navigationController = parentview.navigationController else {
+                return
+            }
+            navigationController.pushViewController(targetView, animated: true)
+        }
+    }
+}
