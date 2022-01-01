1385c8eeff8fd5bb36330532317c1f7e6dc049cd
Aya Hilal <ahilal@identity-solutions.org>
2022-01-01 19:29:27 +0200

    Create structure

---
+++ PhotosListTask/PhotosListTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotosListTask
+//
+//  Created by Aya Mohamed Hilal on 01/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
