69e13502ac32c4042693ae52a06de4e59d4c4889
Aya Hilal <ahilal@identity-solutions.org>
2022-01-04 02:46:40 +0200

    Remove coredata generated files

--- PhotosListTask/PhotosListTask/NetworkLayer/API/PhotosAPI.swift
+++ PhotosListTask/PhotosListTask/NetworkLayer/API/PhotosAPI.swift
@@ -9,7 +9,6 @@ import Foundation
 
 class PhotosAPI: BaseAPI {
     static func getPhotosList(pageNumber: Int, limit: Int, completion: @escaping ([Photo]?, APIError?) -> Void) {
-        
         request(request: PhotosRouter.getPhotosList(pageNumber: pageNumber, limit: limit), responseType: [Photo].self, showDefaultErrorSnackbar: false) { result, error  in
             completion(result, error)
         }
